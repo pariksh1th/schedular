8ed068198d25ae4bb61be53f91143b97f10b668b
Parikshith Palegar <parikshith@Parikshiths-MacBook-Air.local>
2022-06-18 16:03:07 +0530

    deploy commit

--- components/TableCanvas.js
+++ components/TableCanvas.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 export default function TableCanvas() {
   return (
     <div className="hero min-h-[80vh] bg-base-200">
@@ -5,6 +6,7 @@ export default function TableCanvas() {
         <img
           src="https://api.lorem.space/image/movie?w=260&h=400"
           className="max-w-sm rounded-lg shadow-2xl"
+          alt="img"
         />
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">No Timetable Yet!</h1>
